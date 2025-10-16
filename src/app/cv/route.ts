68ed91155201c246860d28e226aa41d2066650ae
almostJohn <garcia.johngale@gmail.com>
2025-10-16 21:10:38 +0800

    refactor: restructure

--- src/app/cv/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-const redirectURL =
-	"https://drive.google.com/file/d/1pNNdFKtTtu2MrmIJAMz1q6pEsXxe5noC/view?usp=drive_link";
-
-export async function GET(_request: NextRequest) {
-	return NextResponse.redirect(redirectURL, 302);
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,24 @@
-import {
-	PageHeader,
-	PageHeaderHeading,
-	PageHeaderDescription,
-} from "@/components/page-header";
-import { SectionList } from "@/components/section-list";
-import { MainNav } from "@/components/main-nav";
-import { MobileNav } from "@/components/mobile-nav";
-
-const projects = [
-	{
-		title: "tsconfig-site",
-		href: "https://tsconfig-site.vercel.app",
-	},
-	{
-		title: "syntra-site",
-		href: "https://syntra-site.vercel.app",
-	},
-	{
-		title: "thoughtsthing-site",
-		href: "https://github.com/almostJohn/thoughtsthing",
-	},
-	{
-		title: "demo-merch-site",
-		href: "https://github.com/almostJohn/demo-merch-site",
-	},
-	{
-		title: "scaffold.js",
-		href: "https://github.com/almostJohn/scaffold.js",
-	},
-	{
-		title: "more? check out repositories",
-		href: "https://github.com/almostJohn?tab=repositories",
-	},
-];
-
 export default function HomePage() {
 	return (
-		<div className="flex flex-col gap-6 mt-14 md:flex-row md:justify-between">
-			<PageHeader>
-				<PageHeaderHeading>almostjohn</PageHeaderHeading>
-				<PageHeaderDescription>
-					i&apos;m 24 y/o software developer based in the philippines. i love
-					building things, shuffling cards, and solving problems. i love
-					language design, and web development. i mostly use{" "}
+		<div className="flex flex-col gap-6">
+			<a
+				href="https://github.com/almostJohn"
+				rel="noreferrer"
+				target="_blank"
+				className="text-2xl sm:text-3xl"
+			>
+				John Gale Garcia
+			</a>
+			<div className="grid gap-4">
+				<p className="text-xl/8 text-pretty">
+					I&apos;m a developer based in the Philippines. I love building things,
+					shuffling cards, and solving problems. I love language design and web
+					development. I mostly use{" "}
 					<a
 						href="https://nextjs.org"
 						rel="noreferrer"
 						target="_blank"
-						className="underline underline-offset-2 decoration-neutral-500 transition-colors hover:decoration-neutral-100"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
 					>
 						next.js
 					</a>
@@ -56,45 +27,84 @@ export default function HomePage() {
 						href="https://react.dev"
 						rel="noreferrer"
 						target="_blank"
-						className="underline underline-offset-2 decoration-neutral-500 transition-colors hover:decoration-neutral-100"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
 					>
 						react
 					</a>
 					.
-				</PageHeaderDescription>
-				<SectionList title="projects">
-					<ul className="mt-2 space-y-2 list-disc list-inside">
-						{projects.map((item, i) => (
-							<li key={i} className="list-item">
-								<a
-									href={item.href}
-									rel="noreferrer"
-									target="_blank"
-									className="text-neutral-400 text-sm transition-colors duration-200 hover:text-neutral-100"
-								>
-									{item.title}
-								</a>
-							</li>
-						))}
-					</ul>
-				</SectionList>
-				<SectionList title="looking for my cv?">
-					<ul className="mt-2 list-disc list-inside">
-						<li className="list-item">
-							<a
-								href="/cv"
-								rel="noreferrer"
-								target="_blank"
-								className="text-neutral-400 text-sm transition-colors duration-200 hover:text-neutral-100"
-							>
-								curriculum vitae
-							</a>
-						</li>
-					</ul>
-				</SectionList>
-			</PageHeader>
-			<MainNav />
-			<MobileNav />
+				</p>
+				<p className="text-xl/8 text-pretty">
+					Previously, I worked at{" "}
+					<a
+						href="https://upskwela.com"
+						rel="noreferrer"
+						target="_blank"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
+					>
+						Upskwela
+					</a>
+					, a startup company that aims to build a web app for content creators
+					in the Philippines to create and grow communities with their fans. I
+					joined the development team and helped move the project forward.
+				</p>
+				<p className="text-xl/8 text-pretty">
+					You can check my{" "}
+					<a
+						href="https://github.com/almostJohn?tab=repositories"
+						rel="noreferrer"
+						target="_blank"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
+					>
+						projects here
+					</a>
+					, or follow me online in my socials such as,{" "}
+					<a
+						href="https://x.com/almostJohn1"
+						rel="noreferrer"
+						target="_blank"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
+					>
+						x (formerly twitter)
+					</a>
+					,{" "}
+					<a
+						href="https://instagram.com/almostjohn1"
+						rel="noreferrer"
+						target="_blank"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
+					>
+						instagram
+					</a>
+					,{" "}
+					<a
+						href="https://facebook.com/alsojohn01"
+						rel="norefferer"
+						target="_blank"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
+					>
+						facebook
+					</a>
+					, and{" "}
+					<a
+						href="https://discord.com/users/996354867708841984"
+						rel="noreferrer"
+						target="_blank"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
+					>
+						discord
+					</a>
+					. I also make projects for commissions.{" "}
+					<a
+						href="mailto:garcia.johngale@gmail.com"
+						rel="noreferrer"
+						target="_blank"
+						className="underline decoration-neutral-500 hover:decoration-neutral-100"
+					>
+						Reach out
+					</a>{" "}
+					if interested.
+				</p>
+			</div>
 		</div>
 	);
 }
